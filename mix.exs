82ee557c7ebaa36af96c19bb1c47136480cf4ac6
Jechol Lee <mr.jechol@gmail.com>
2020-09-18 12:31:46 +0900

    Add hex info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
-defmodule TimeoutBuffer.MixProject do
+defmodule Buffered.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :buffered,
       version: "0.1.0",
-      elixir: ">= 1.6.0",
+      elixir: ">= 1.7.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "Buffered queue and counter",
+      source_url: "https://github.com/jechol/buffered",
+      docs: docs()
     ]
   end
 
@@ -21,8 +25,29 @@ defmodule TimeoutBuffer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.21.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jechol/buffered"},
+      maintainers: ["Jechol Lee(mr.jechol@gmail.com)"]
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "buffered",
+      canonical: "http://hexdocs.pm/buffered",
+      source_url: "https://github.com/jechol/buffered",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
--- test/timeout_buffer_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TimeoutBufferTest do
-  use ExUnit.Case
-  doctest TimeoutBuffer
-
-  test "greets the world" do
-    assert TimeoutBuffer.hello() == :world
-  end
-end
